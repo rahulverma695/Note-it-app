bea95c1e3441de9020e14a18aabe23e1efd3f46f
rahulverma695 <86546697+rahulverma695@users.noreply.github.com>
2021-06-27 22:13:00 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import WbIncandescentSharpIcon from "@material-ui/icons/WbIncandescentSharp";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <WbIncandescentSharpIcon />
+        Noteit
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
